eff42608744e2895320b09ff3efec2fbd1204f78
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-03-07 20:59:46 +0100

    Add files via upload

---
+++ main43.py
@@ -0,0 +1,6 @@
+def display_favorite_sport(sport):
+    print("Twój ulubiony sport to:", sport)
+
+if __name__ == "__main__":
+    sport = input("Jaki jest Twój ulubiony sport? ")
+    display_favorite_sport(sport)
